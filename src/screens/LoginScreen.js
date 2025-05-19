55a6f449d679f4d36e32c5728e71aa776a31bbdb
Otacz <ota.g@seznam.cz>
2025-05-19 13:38:34 +0200

    Add files via upload

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -16,13 +16,12 @@ const LoginScreen = () => {
   };
 
   return (
-{showInstallButton && (
-  <button onClick={handleInstallClick} style={{ marginTop: '1rem', padding: '0.5rem', background: '#00ffff', border: 'none', borderRadius: '4px', color: '#000' }}>
-    Nainstalovat GeriApp
-  </button>
-)}
 
-
+      {showInstallButton && (
+        <button onClick={handleInstallClick} style={{ marginTop: "1rem", padding: "0.5rem", background: "#00ffff", border: "none", borderRadius: "4px", color: "#000" }}>
+          Nainstalovat GeriApp
+        </button>
+      )}
     <div style={{
       display: "flex",
       flexDirection: "column",
